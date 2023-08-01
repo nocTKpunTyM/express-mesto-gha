e8e384b146a943b7ad76020f576c852d9aa910df
nocTKpunTyM <noctkpuntym@yandex.ru>
2023-08-01 10:30:34 +0300

    Change API adress

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const NotFoundError = require('./errors/not-found-err');
 const { signinValidation, signupValidation } = require('./middlewares/celebrate');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 mongoose.connect(DB_URL);
 
 const app = express();
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 app.use(helmet());
 const limiter = rateLimit({
@@ -21,6 +23,13 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
@@ -29,6 +38,7 @@ const auth = require('./middlewares/auth');
 const errorsMid = require('./middlewares/errors');
 
 app.use(bodyParser.json());
+app.use(requestLogger); // подключаем логгер запросов
 
 app.post('/signin', signinValidation, login);
 app.post('/signup', signupValidation, createUser);
@@ -37,6 +47,8 @@ app.use('/cards', auth, cardsRouter);
 app.use((req, res, next) => {
   next(new NotFoundError({ message: 'Страница не найдена' }));
 });
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsMid);
 app.listen(PORT);
